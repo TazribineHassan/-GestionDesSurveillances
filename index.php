93f5e6cdb0d188b89c601fe603f15c63e60c6075
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2021-04-05 00:33:32 +0200

    merging

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php 
-<<<<<<< HEAD
-
-require_once("./Views/includes/header.php");
-require("Controllers/HomeControllers.php");
-$pages = ["index"];
-
-$home = new HomeController();
-
-if(isset($_GET["page"])){
-    $page = $_GET["page"];
-    if(in_array($page, $pages)){
-        $home->index($page);
-    }else{
-        echo "page not";
-    }
-}else{
-    $home->index("index");
-}
-=======
 session_start();
 header("Location: Controllers/indexControllers.php");
 ?>
->>>>>>> 3ec5f31b4199e84a762fef397ace878b8b63c144
