62585620a46d0eef5fcf615021fa7cad99e05b23
Tazribine Hassan <tazribinehassan1998@gmail.com>
2021-04-04 20:55:27 +0100

    start the project

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+require("Controllers/HomeControllers.php");
+$pages = ["index"];
+
+$home = new HomeController();
+
+if(isset($_GET["page"])){
+    $page = $_GET["page"];
+    if(in_array($page, $pages)){
+        $home->index($page);
+    }else{
+        echo "page not";
+    }
+}else{
+    
+}
