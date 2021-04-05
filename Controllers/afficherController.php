9dbbd12dae2adc533c24bbdf30b46b9f613f9574
Tazribine Hassan <tazribinehassan1998@gmail.com>
2021-04-05 01:39:33 +0100

    added ajax

--- Controllers/afficherController.php
+++ Controllers/afficherController.php
@@ -1,4 +1,13 @@
 <?php 
 
+include("../Models/connection.php");
+$conn = new ConnectionClass();
+$profs = $conn->SelectAllFromTable("prof");
 
 ?>
+
+<table class="table">
+    <tr>
+        <th></th>
+    </tr>
+</table>
